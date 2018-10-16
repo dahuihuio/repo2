c9c7d89be1891e1922f32cc4525b7811697e0c4f
hasaki <hasaki@qq.com>
2018-10-16 19:50:13 +0800

    添加gitTest工程添加到git

---
+++ gitTest/src/com/itheima/HelloGit.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+    }
+
+}
